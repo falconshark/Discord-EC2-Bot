d4939e1e8ab7eb2444aa51151e630d290582d536
Mr.Twister <sardo.ip@sardo.work>
2022-11-23 10:41:11 +0800

    Start rewrite to new discord API

--- discord_bot.js
+++ discord_bot.js
@@ -10,56 +10,74 @@ const config = JSON.parse(configFile);
 
 //By default will check server had user each hour
 const checkServerTime = config['checkServerTime'];
-
 const discordToken = config['discordBotToken'];
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const discordClientId = config['discordClientId'];
 
+const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+//Register
+const commands = [
+	{
+		name: 'status',
+		description: '檢查伺服器狀態',
+	},
+  {
+    name: 'start',
+    description: '啟動伺服器',
+  },
+  {
+    name: 'stop',
+    description: '停止伺服器',
+  },
+  {
+    name: 'choose',
+    description: '(選項) 機器人老師，幫我選擇！格式範例：選項1|選項2。',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(discordToken);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+		await rest.put(Routes.applicationCommands(discordClientId), { body: commands });
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+/*
 setInterval(function () {
   _checkValheimStatus();
 }, checkServerTime);
+*/
 
-//If the bot had been connected, show message
+//Real part for bot action
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
-//Receive message
-client.on('message', msg => {
-    const command = Message.readCommand(msg.content);
-    switch(command){
-      case 'help':
-      _getHelp(msg);
-      break;
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
 
-      case 'status':
-      _checkStatus(msg);
-      break;
-
-      case 'start':
-      _startServer(msg);
-      break;
-
-      case 'stop':
-      _stopServer(msg);
-      break;
-
-      case 'choose':
-      _choose(msg);
-      break;
-    }
+	if (interaction.commandName === 'status') {
+    _checkStatus(interaction);
+	}
+  if (interaction.commandName === 'start') {
+    _startServer(interaction);
+  }
+  if (interaction.commandName === 'stop') {
+    _stopServer(interaction);
+  }
+  if (interaction.commandName === 'choose') {
+    _choose(interaction);
+  }
 });
 
 client.login(discordToken);
-
-function _getHelp(msg){
-  let content = '目前指令：';
-  content += '\n /status 檢查伺服器狀態';
-  content += '\n /start 啟動伺服器';
-  content += '\n /stop 停止伺服器';
-  content += '\n /choose (選項) 機器人老師，幫我選擇！格式範例：選項1|選項2。';
-  msg.reply(content);
-}
 
 async function _checkStatus(msg){
   const status = await Aws.checkStatus(request, config);
@@ -71,33 +89,20 @@ async function _checkStatus(msg){
   }else{
     content += '尚未啟動或正在啟動（¯﹃¯）';
   }
-  msg.reply(content);
+  await msg.reply(content);
 }
 
 async function _startServer(msg){
+  console.log('Work Here?');
   const startResult = await Aws.startServer(request, config);
   let content = '遊戲伺服器正在啟動。需時大約5-7分鐘左右！（請耐心等侯，維京人就是啟動得比較慢）';
-  msg.reply(content);
+  await msg.reply(content);
 }
 
 async function _stopServer(msg){
   const stopResult = await Aws.stopServer(request, config);
   let content = '遊戲伺服器正在關閉。晚安✧*｡٩(ˊᗜˋ*)و✧*｡ ';
-  msg.reply(content);
-}
-
-function _choose(msg){
-  const chooseRegax = /^(\/choose) ([\s\S]*)$/;
-  const optionsString = chooseRegax.exec(msg)[2];
-  const options = optionsString.split('|');
-  let content = '';
-  if(options.length !== 0){
-    const result = Common.choose(options);
-    content = '隨機選擇結果：' + result;
-  }else{
-    content = '格式不符合！';
-  }
-  msg.reply(content);
+  await msg.reply(content);
 }
 
 async function _checkValheimStatus(){
@@ -105,7 +110,7 @@ async function _checkValheimStatus(){
   //If server do not had any player, stop the ec2 server
   const serverStatus = await Common.checkValheimStatus(request, statusUrl);
   if(serverStatus === 'non_playing'){
-    console.log('Cannot detect any player. Stop Server...');
-    await Aws.stopServer(request, config);
+    //console.log('Cannot detect any player. Stop Server...');
+    //await Aws.stopServer(request, config);
   }
 }
