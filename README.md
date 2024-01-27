a947fdedb833dda7e5b7ba1ea8d1030a02e01dcc
Sardo <sardo.ip@sardo.work>
2024-01-27 15:49:29 +0800

    Rewrite to new version.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Discord EC2 BOT
+Using discord to control a EC2 server.
+
+## Requirement
+Node.js version 16.18.0 or higher.
+
+## Usage and Deploy
+1. Copy config.sample.json to config.json, edit it.
+```json
+{
+	"accessKeyId": "Access Key for AWS",
+	"secretAccessKey": "Access Key Secret for AWS",
+	"instanceId": "EC2 instance ID",
+	"region": "ap-east-1",
+  "discordBotToken": "Discord Bot Token",
+  "discordClientId": "Discord Bot Client ID"
+}
+```
+2. Install PM2 (Advanced process manager) for keep running it:
+```bash
+npm install pm2 -g`
+pm2 start discord_bot.js
+pm2 save
+```
--- discord_bot.js
+++ discord_bot.js
@@ -1,22 +1,24 @@
 const fs = require('fs');
 const path = require('path');
-const request = require('request');
+const AwsSdk = require("aws-sdk");
+
 const Common = require('./lib/common');
-const Message = require('./lib/message');
 const Aws = require('./lib/aws');
 
 const configFile = fs.readFileSync(path.resolve(__dirname, './config.json'), 'utf8');
 const config = JSON.parse(configFile);
 
-//By default will check server had user each hour
-const checkServerTime = config['checkServerTime'];
 const discordToken = config['discordBotToken'];
 const discordClientId = config['discordClientId'];
 
 const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-//Register
+AwsSdk.config.loadFromPath('./config.json');
+
+const ec2 = new AwsSdk.EC2({ apiVersion: "2016-11-15" });
+
+//設定Disocrd的Command，需要加新功能才改這裡
 const commands = [
 	{
 		name: 'status',
@@ -32,6 +34,7 @@ const commands = [
   },
 ];
 
+// 不用理會
 const rest = new REST({ version: '10' }).setToken(discordToken);
 
 (async () => {
@@ -44,16 +47,12 @@ const rest = new REST({ version: '10' }).setToken(discordToken);
 	}
 })();
 
-setInterval(function () {
-  _checkValheimStatus();
-}, checkServerTime);
-
-//Real part for bot action
-
+// 不用理會
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
+//偵測用戶輸入Command => Function
 client.on('interactionCreate', async (interaction) => {
 	if (!interaction.isChatInputCommand()) return;
 
@@ -71,36 +70,25 @@ client.on('interactionCreate', async (interaction) => {
 client.login(discordToken);
 
 async function _checkStatus(msg){
-  const status = await Aws.checkStatus(request, config);
-
+  const status = await Aws.checkStatus(ec2, config);
   let content = '遊戲伺服器目前狀態: ';
 
-  if(status === 'running'){
-    content += '正在運行✧◝(⁰▿⁰)◜✧';
+  if(!status){
+		content += '尚未啟動或正在啟動（¯﹃¯）';
   }else{
-    content += '尚未啟動或正在啟動（¯﹃¯）';
+    content += '正在運行✧◝(⁰▿⁰)◜✧';
   }
   await msg.reply(content);
 }
 
 async function _startServer(msg){
-  const startResult = await Aws.startServer(request, config);
-  let content = '遊戲伺服器正在啟動。需時大約5-7分鐘左右！（請耐心等侯，維京人就是啟動得比較慢）';
+  const startResult = await Aws.startServer(ec2, config);
+  let content = '遊戲伺服器正在啟動。需時大約2-3分鐘左右！';
   await msg.reply(content);
 }
 
 async function _stopServer(msg){
-  const stopResult = await Aws.stopServer(request, config);
+  const stopResult = await Aws.stopServer(ec2, config);
   let content = '遊戲伺服器正在關閉。晚安✧*｡٩(ˊᗜˋ*)و✧*｡ ';
   await msg.reply(content);
-}
-
-async function _checkValheimStatus(){
-  const statusUrl = config['valheimServerStatusUrl'];
-  //If server do not had any player, stop the ec2 server
-  const serverStatus = await Common.checkValheimStatus(request, statusUrl);
-  if(serverStatus === 'non_playing'){
-    console.log('Cannot detect any player. Stop Server...');
-    await Aws.stopServer(request, config);
-  }
 }
